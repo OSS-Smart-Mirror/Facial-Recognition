5107c9ab1a130ddd5f4618e18653eb14020738a0
Rtvik Sriram Bharadwaj <rbharadwaj71@gmail.com>
2019-09-09 22:10:12 -0400

    .

--- detect_face.py
+++ detect_face.py
@@ -7,13 +7,13 @@ from time import sleep, gmtime, strftime, time
 def find_face_from_camera_feed(cascPath="haarcascade_frontalface_default.xml"):
     faceCascade = cv2.CascadeClassifier(cascPath)
     video_capture = cv2.VideoCapture(0)
+    sleep(0.5)
     start_time = time()
     while True:
-        sleep(0.2)
-        if (time() - start_time) > 5:
-            return None, "Time limit exceeded, could not find a face in the video stream"
+        if (time() - start_time) > 30:
+            raise TimeoutError("Operation timed out")
         elif not video_capture.isOpened():
-            return None, "OpenCV could not open the webcam, please check your configurations"
+            raise cv2.error("Camera could not be opened")
         else:
             ret, frame = video_capture.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -24,26 +24,36 @@ def find_face_from_camera_feed(cascPath="haarcascade_frontalface_default.xml"):
                 minSize=(30, 30)
             )
             for f in faces:
-                timestamp = strftime("%H:%M:%S", gmtime()) + ".png"
-                x, y, w, h = [ v for v in f ]
-                cv2.rectangle(frame, (x,y), (x+w,y+h), (255,255,255))
-                sub_face = frame[y:y+h, x:x+w]
-                cv2.imwrite(timestamp, sub_face)
-                return timestamp, None
+                user = None
+                try:
+                    image_name = strftime("%H:%M:%S", gmtime()) + ".png"
+                    x, y, w, h = [ v for v in f ]
+                    cv2.rectangle(frame, (x,y), (x+w,y+h), (255,255,255))
+                    sub_face = frame[y:y+h, x:x+w]
+                    cv2.imwrite(image_name, sub_face)
+                    p = Popen(['face_recognition', 'known_users', image_name], stdout=PIPE)
+                    output = p.communicate()[0]
+                    if (p.returncode != 0):
+                       raise SystemError("Non-zero exit code from face recognition module")
+                    elif "no_persons_found" in output.decode("utf-8"):
+                        continue
+                    else:
+                        return output.decode("utf-8").split(',')[1], None
+                except (Warning, Exception) as e:
+                    return None, e
+                finally:
+                    os.remove(image_name)
 
-if __name__ == "__main__":
-    image_name, error_message = find_face_from_camera_feed()
-    if error_message is not None:
-        print(error_message)
-    else:
-        try:
-            p = Popen(['face_recognition', 'known_users', image_name], stdout=PIPE)
-            output = p.communicate()[0]
-            if p.returncode != 0:
-               raise SystemError("Non-zero exit code from face recognition module")
-            print(output.decode("utf-8").split(',')[1])
-        except (Warning, Exception) as e:
-            print(e)
-        finally:
-            pass
-            # os.remove(image_name)
+# def run_face_detect():
+#     image_name = find_face_from_camera_feed()
+#     p = Popen(['face_recognition', 'known_users', image_name], stdout=PIPE)
+#     output = p.communicate()[0]
+#     if p.returncode != 0:
+#        raise SystemError("Non-zero exit code from face recognition module")
+#     return output.decode("utf-8").split(',')[1]
+#     os.remove(image_name)
+
+def detect_face():
+    print(find_face_from_camera_feed())
+
+detect_face()
