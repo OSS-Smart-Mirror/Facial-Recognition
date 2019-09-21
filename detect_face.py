e3fcbedce2f78bd3f8ceec08abc944e860ec1d8d
Rtvik Sriram Bharadwaj <rbharadwaj71@gmail.com>
2019-09-21 16:55:12 -0400

    adding flask api for the face detect

--- detect_face.py
+++ detect_face.py
@@ -1,59 +1,82 @@
-import cv2
-import sys
+import face_recognition
 import os
-from subprocess import Popen, PIPE
-from time import sleep, gmtime, strftime, time
+import cv2
+import numpy as np
+from flask import Flask
 
-def find_face_from_camera_feed(cascPath="haarcascade_frontalface_default.xml"):
-    faceCascade = cv2.CascadeClassifier(cascPath)
-    video_capture = cv2.VideoCapture(0)
-    sleep(0.5)
-    start_time = time()
-    while True:
-        if (time() - start_time) > 30:
-            raise TimeoutError("Operation timed out")
-        elif not video_capture.isOpened():
-            raise cv2.error("Camera could not be opened")
-        else:
-            ret, frame = video_capture.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = faceCascade.detectMultiScale(
-                gray,
-                scaleFactor=1.1,
-                minNeighbors=5,
-                minSize=(30, 30)
-            )
-            for f in faces:
-                user = None
-                try:
-                    image_name = strftime("%H:%M:%S", gmtime()) + ".png"
-                    x, y, w, h = [ v for v in f ]
-                    cv2.rectangle(frame, (x,y), (x+w,y+h), (255,255,255))
-                    sub_face = frame[y:y+h, x:x+w]
-                    cv2.imwrite(image_name, sub_face)
-                    p = Popen(['face_recognition', 'known_users', image_name], stdout=PIPE)
-                    output = p.communicate()[0]
-                    if (p.returncode != 0):
-                       raise SystemError("Non-zero exit code from face recognition module")
-                    elif "no_persons_found" in output.decode("utf-8"):
-                        continue
-                    else:
-                        return output.decode("utf-8").split(',')[1], None
-                except (Warning, Exception) as e:
-                    return None, e
-                finally:
-                    os.remove(image_name)
+app = Flask(__name__)
 
-# def run_face_detect():
-#     image_name = find_face_from_camera_feed()
-#     p = Popen(['face_recognition', 'known_users', image_name], stdout=PIPE)
-#     output = p.communicate()[0]
-#     if p.returncode != 0:
-#        raise SystemError("Non-zero exit code from face recognition module")
-#     return output.decode("utf-8").split(',')[1]
-#     os.remove(image_name)
+abhay_image_1 = face_recognition.load_image_file("known_users/Abhay,1.jpg")
+abhay_face_encoding_1 = face_recognition.face_encodings(abhay_image_1)[0]
+abhay_image_2 = face_recognition.load_image_file("known_users/Abhay,2.jpg")
+abhay_face_encoding_2 = face_recognition.face_encodings(abhay_image_2)[0]
+prat_image_1 = face_recognition.load_image_file("known_users/Prat,1.jpg")
+prat_face_encoding_1 = face_recognition.face_encodings(prat_image_1)[0]
+prat_image_2 = face_recognition.load_image_file("known_users/Prat,2.jpg")
+prat_face_encoding_2 = face_recognition.face_encodings(prat_image_2)[0]
+rtvik_image_1 = face_recognition.load_image_file("known_users/Rtvik,1.jpg")
+rtvik_face_encoding_1 = face_recognition.face_encodings(rtvik_image_1)[0]
+rtvik_image_2 = face_recognition.load_image_file("known_users/Rtvik,2.jpg")
+rtvik_face_encoding_2 = face_recognition.face_encodings(rtvik_image_2)[0]
+# ishaan_image_1 = face_recognition.load_image_file("known_users/Ishaan,1.jpg")
+# ishaan_face_encoding_1 = face_recognition.face_encodings(ishaan_image_1)[0]
+# ishaan_image_2 = face_recognition.load_image_file("known_users/Ishaan,2.jpg")
+# ishaan_face_encoding_2 = face_recognition.face_encodings(ishaan_image_2)[0]
 
+known_face_encodings = [
+   abhay_face_encoding_1,
+   abhay_face_encoding_2,
+   prat_face_encoding_1,
+   prat_face_encoding_2,
+   rtvik_face_encoding_1,
+   rtvik_face_encoding_2,
+   # ishaan_face_encoding_1,
+   # ishaan_face_encoding_2,
+]
+
+known_face_names = [
+   "Abhay",
+   "Abhay",
+   "Prat",
+   "Prat",
+   "Rtvik",
+   "Rtvik",
+   # "Ishaan",
+   # "Ishaan"
+]
+face_locations = []
+face_encodings = []
+face_names = []
+
+@app.route('/', methods=['GET'])
 def detect_face():
-    print(find_face_from_camera_feed())
+# Detect Face Function, returns (name/error, success/failure)
+   try:
+       video_capture = cv2.VideoCapture(0)
+       process_this_frame = True
+       while True:
+           ret, frame = video_capture.read()
+           small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+           cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+           rgb_small_frame = small_frame[:, :, ::-1]
+           if process_this_frame:
+               face_locations = face_recognition.face_locations(rgb_small_frame)
+               face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+               face_names = []
+               for face_encoding in face_encodings:
+                   matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                   name = "Unknown"
+                   face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                   best_match_index = np.argmin(face_distances)
+                   if matches[best_match_index]:
+                       name = known_face_names[best_match_index]
+                       return (name)
+                   face_names.append(name)
+           process_this_frame = not process_this_frame
+   except (Warning, Exception) as e:
+       return str(e), False
+   finally:
+       video_capture.release()
 
-detect_face()
+if __name__ == "__main__":
+   app.run(debug=True, host='127.0.0.1', port=8080)
